f77f81bed733d5b3ec0bfdccb9e6012417d2d3fc
sdv <56010236+lucky414-ux@users.noreply.github.com>
2026-08-12 14:01:41 +0530

    Create main.jl

---
+++ main.jl
@@ -0,0 +1,8 @@
+println("Hello from JuliaHub!")
+
+numbers = 1:10
+total = sum(numbers)
+
+println("Numbers: ", collect(numbers))
+println("Total: ", total)
+println("JuliaHub demo successfully ran!")
